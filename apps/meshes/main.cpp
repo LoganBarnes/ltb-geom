d4cd1cbfa8f8a0045749fb656da440d1c4ab85be
Logan Barnes <barneslt63@gmail.com>
2022-04-09 22:38:22 -0400

    Fix build on linux

--- apps/meshes/main.cpp
+++ apps/meshes/main.cpp
@@ -1,20 +1,20 @@
 // ///////////////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2022 Logan Barnes - All Rights Reserved
 // ///////////////////////////////////////////////////////////////////////////////////////
-
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
-#include <spdlog/spdlog.h>
+#ifdef DOCTEST_CONFIG_DISABLE
 
 #include "ltb/app/application_data.hpp"
 #include "ltb/app/glfw_vulkan_imgui_app.hpp"
 
 #include <doctest/doctest.h>
+#include <spdlog/spdlog.h>
 
 class HelloTriangleApp : public ltb::app::GlfwVulkanImguiApp {
 public:
     ~HelloTriangleApp() override = default;
 
 private:
+    auto init() -> void override {}
     auto update() -> void override {}
     auto render() -> void override {}
     auto resized() -> void override {}
@@ -22,6 +22,7 @@ private:
 };
 
 auto main() -> int {
+
     spdlog::set_level(spdlog::level::debug);
 
     // auto extensions = HelloTriangleApp::get_extensions();
@@ -44,3 +45,10 @@ auto main() -> int {
 
     return EXIT_SUCCESS;
 }
+
+#else
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#endif
