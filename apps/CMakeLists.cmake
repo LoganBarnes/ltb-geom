fbd66f1ae601cddc0fbc6b66bbfe71ee3a22885e
Logan Barnes <logan@flyrotor.com>
2022-05-19 23:51:49 -0400

    Refactor into src and include dirs

--- apps/CMakeLists.cmake
+++ apps/CMakeLists.cmake
@@ -1,24 +1,20 @@
-##########################################################################################
+# ##############################################################################
 # Copyright (c) 2022 Logan Barnes - All Rights Reserved
-##########################################################################################
+# ##############################################################################
 
-#########################
-### External Packages ###
-#########################
-CPMAddPackage(
-        NAME LtbApp
-        GITHUB_REPOSITORY LoganBarnes/ltb-app
-)
+# ##############################################################################
+# External Packages
+# ##############################################################################
+cpmaddpackage(NAME
+              LtbApp
+              GITHUB_REPOSITORY
+              LoganBarnes/ltb-app
+              GIT_TAG
+              main
+              )
 
-############
-### Apps ###
-############
-ltb_create_app(
-        meshes-app
-        ${CMAKE_CURRENT_LIST_DIR}/meshes/main.cpp
-)
-target_link_libraries(
-        meshes-app
-        PRIVATE
-        LtbGeom::LtbGeom
-)
+# ##############################################################################
+# Apps
+# ##############################################################################
+ltb_create_app(ltb_geom_meshes_app ${CMAKE_CURRENT_LIST_DIR}/meshes/main.cpp)
+target_link_libraries(ltb_geom_meshes_app PRIVATE LtbGeom::LtbGeom)
